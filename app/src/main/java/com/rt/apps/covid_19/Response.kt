416a9d86f49d1341fdf2674625a5b3da7a57b9f0
Rahul Tilwani <tilwani03@gmail.com>
2020-07-02 16:44:07 +0530

    UI changes

--- app/src/main/java/com/rt/apps/covid_19/Response.kt
+++ app/src/main/java/com/rt/apps/covid_19/Response.kt
@@ -1,20 +1,20 @@
 package com.rt.apps.covid_19
 
 data class Response(
-	val statewise: List<StatewiseItem>
+    val statewise: List<StatewiseItem>
 )
 
 data class StatewiseItem(
-	val deltarecovered: String? = null,
-	val deltaactive: String? = null,
-	val deltaconfirmed: String? = null,
-	val deltadeaths: String? = null,
+    val deltarecovered: String? = null,
+    val deltaactive: String? = null,
+    val deltaconfirmed: String? = null,
+    val deltadeaths: String? = null,
 
-	val recovered: String? = null,
-	val active: String? = null,
-	val state: String? = null,
-	val confirmed: String? = null,
-	val deaths: String? = null,
-	val lastupdatedtime: String? = null
+    val recovered: String? = null,
+    val active: String? = null,
+    val state: String? = null,
+    val confirmed: String? = null,
+    val deaths: String? = null,
+    val lastupdatedtime: String? = null
 )
 
